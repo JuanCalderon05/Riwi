3e0a907772a049d078a3dd215cf5a41adb608eaf
JuanCalderon05 <120690594+JuanCalderon05@users.noreply.github.com>
2024-02-22 16:42:06 -0500

    Add files via upload

---
+++ notas promedio final.py
@@ -0,0 +1,11 @@
+Dat_1 = float(input("Nota 1:" ))
+Dat_2 = float(input("Nota 2:" ))
+Dat_3= float(input("Nota 3:" ))
+
+prom_ = (Dat_1+Dat_2+Dat_3)/3
+
+if prom_ <= 2.9:
+    print("PERDISTE LA MATERIA3 :()")
+else:
+    print("PASASTE LA MATERIA :)")
+    
