69f88733ff9ef721c7a95a2a3cb721a2d4bc1f80
JuanCalderon05 <120690594+JuanCalderon05@users.noreply.github.com>
2024-02-22 15:08:44 -0500

    Add files via upload

---
+++ cono.py
@@ -0,0 +1,4 @@
+cAlt_vol = float(input("ingresa porfavor la altura del cono:" ))
+cap_vol = float(input("ingresa porfavor el radio del cono:" ))
+Vol_cono = (3.1415 * (cap_vol**2)*cAlt_vol)/3
+print("el volumen del cono es: ",Vol_cono,"m³")
